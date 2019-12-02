80484c4758e761abe10cb6a303ccee57a912f152
NajiyaShaju <najiya.shajifrq@gmail.com>
2019-12-02 13:13:25 +0530

    first commit

---
+++ src/main/java/com/myproject/smple1/Smple1Application.java
@@ -0,0 +1,13 @@
+package com.myproject.smple1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smple1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Smple1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/smple1/Smple1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.smple1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Smple1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
